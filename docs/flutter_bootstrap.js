398cf90ba9dcddd33bf2b173085c792f71b301f2
zayron <zayrondomingues@gmail.com>
2026-06-28 01:14:54 -0300

    fix

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1395384691" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "508113599" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
